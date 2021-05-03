f525e7bb065abea2be242309149c7903d80a09ca
George Dilthey <george.dilthey@gmail.com>
2021-05-02 22:52:40 -0400

    commit tree c856113123fd74f7cd4d5e1a8104317f4e59efed

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize(name, weight, height)
+        @name, @weight, @height = name, weight, height
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
